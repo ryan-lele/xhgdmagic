cd2c819755a2495dd46ad0cec8dd7680423d8f02
ryan-lele <fx1234310@gmail.com>
2025-09-03 12:19:49 +0800

    Delete Calm风格哄睡应用 (1) directory

--- Calm风格哄睡应用 (1)/src/lib/lumi.ts
+++
@@ -1,9 +0,0 @@
-
-import { createClient } from '@lumi.new/sdk'
-
-export const lumi = createClient({
-  projectId: 'p353722882457477120',
-  apiBaseUrl: 'https://api.lumi.new',
-  authOrigin: 'https://auth.lumi.new',
-})
-
